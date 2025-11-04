4d8176d097c79f46dc043c449f6a88edbe821ffa
Christopher Sasias <christophersasias@hotmail.com>
2025-11-04 02:54:30 +0000

    testing full Jest

---
+++ app.js
@@ -0,0 +1,34 @@
+// Declaramos una función con el nombre exacto "formEuroToDollar"
+const fromEuroToDollar = function (valueInEuro) {
+    // Convertimos el valor a dólares
+    let valueInDollar = valueInEuro * 1.07;
+    // Retornamos el valor en dólares
+    return valueInDollar;
+}
+
+const fromDollarToYen = function (valueInDollar) {
+    
+    let valueInYens = valueInDollar * (156.5 / 1.07);
+
+    return valueInYens;
+}
+
+const fromYenToPound = function (valueInYens) {
+    
+    let valueInPound = valueInYens * (0.87 / 156.5);
+    
+    return valueInPound;
+}
+
+
+
+
+const sum = (a, b) => {
+
+    return a + b
+
+}
+
+console.log(sum(7, 3))
+
+module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound }
